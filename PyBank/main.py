8c55b656146be91b6e75e6ea777b44b145f67644
khadiza7 <92734124+khadiza7@users.noreply.github.com>
2022-02-08 19:43:45 +0000

    Final version

--- PyBank/main.py
+++ PyBank/main.py
@@ -6,7 +6,7 @@ months = []
 net_total = []
 profit_change = []
 
-#Path from the code to the data
+#Path to the data
 csv_path = os.path.join('budget_data.csv')
 
 #Read the file
@@ -28,7 +28,8 @@ total_month = (len(months))
 #Calculate the net total of profit/losses by summing the list
 total_net = sum(net_total)
 
-#Change in profit/loss between each month
+#Change in profit/loss between each month (-1 as there are only 85 calculations required)
+#[x+1] - [x] is the next month minus the previous month to find the change
 for x in range(len(net_total)-1):
         profit_change.append(net_total[x+1] - net_total[x])
 
@@ -36,11 +37,13 @@ for x in range(len(net_total)-1):
 average_change = sum(profit_change) / len(profit_change)
 
 #Greatest increase in profits (date and amount) over entire period
+#+1 is required as the index will show the previous month
 increase = max(profit_change)
 date_max = profit_change.index(increase)+1
 
 
 #Greatest decrease in profits (date and amount) over entire period
+#+1 is required as the index will show the previous month
 decrease = min(profit_change)
 date_min = profit_change.index(decrease)+1
 
@@ -54,7 +57,7 @@ print(f"Greatest Increase in Profits: {months[date_max]} (${increase})")
 print(f"Greatest Decrease in Profits: {months[date_min]} (${decrease})")
 
 
-#Write a text file of the results
+#Write a text file of the results, note: 'write' used instead of 'writerow' for text file.
 with open("Analysis.txt", "w") as Analysis:
     Analysis.write("Financial Analysis\n")
     Analysis.write("-----------------------------------\n")
